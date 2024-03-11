cac35378eb552b3c178ba87b84a15d56808f3542
Matodor <matodor-zlo@yandex.ru>
2024-03-11 14:27:04 +0400

    wip

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// "use strict";
+
+// const teeRenderer = function (container, config = null) {
+//     return new TeeRenderer(container, config || {});
+// }
+
+// class TeeRenderer {
+//     constructor(container, config) {
+//         let colorBody = teeContainer.dataset.colorBody;
+//         let colorFeet = teeContainer.dataset.colorFeet;
+
+//         let skinUrl = teeContainer.dataset.skin;
+//         let skinBitmap = null;
+//         let skinLoading = false;
+//         let skinLoadedCallback = null;
+
+//         let offscreen = null;
+//         let offscreenContext = null;
+//         let loadSkinPromise = null;
+//         let loadSkinCallback = null;
+//     }
+// }
+
+// function domReady(fn, args) {
+//     args = args !== undefined ? args : [];
+
+//     if (document.readyState !== 'loading') {
+//         fn(...args);
+//     } else {
+//         document.addEventListener('DOMContentLoaded', () => {
+//             fn(...args);
+//         });
+//     }
+// }
+
+
