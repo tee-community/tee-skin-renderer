3f9c5ac18a749c5986ebb9740ac47e831056d35c
Matodor <matodor-zlo@yandex.ru>
2024-03-16 14:16:34 +0400

    update: updated vite.config (output css to tee-skin-renderer.css)

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,18 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     build: {
-        copyPublicDir: false,
+        copyPublicDir: true,
         emptyOutDir: true,
-        minify: false,
+        minify: true,
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'TeeSkinRenderer',
             fileName: 'tee-skin-renderer',
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: "tee-skin-renderer.[ext]",
+            },
         },
     },
     resolve: {
@@ -25,17 +30,27 @@ export default defineConfig({
             generateBundle() {
                 this.emitFile({
                     type: 'asset',
-                    fileName: 'index.html',
-                    source: fs.readFileSync(resolve(__dirname, 'index.dist.html'), 'utf-8'),
+                    fileName: 'demo.umd.html',
+                    source: fs.readFileSync(resolve(__dirname, 'demo/demo.umd.html'), 'utf-8'),
+                });
+
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'demo.html',
+                    source: fs.readFileSync(resolve(__dirname, 'demo/demo.html'), 'utf-8'),
+                });
+
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'demo.css',
+                    source: fs.readFileSync(resolve(__dirname, 'demo/demo.css'), 'utf-8'),
                 });
             },
         },
         dts({
             include: ['src'],
             outDir: ['dist'],
-            // staticImport: true,
             rollupTypes: true,
-            // declarationOnly: true,
         }),
     ],
 });
